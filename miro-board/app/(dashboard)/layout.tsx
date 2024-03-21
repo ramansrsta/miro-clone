18d38e2c6e410254a3a2ea12b10a904ebc86d206
rmnsrsta <ramanshrestha@varicon.com.au>
2024-03-21 10:13:47 +0545

    Side bar Completed

---
+++ miro-board/app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import { Navbar } from "./_components/navbar";
+import { OrgSidebar } from "./_components/org-sidebar";
+import { Sidebar } from "./_components/sidebar";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex h-full gap-x-3">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default DashboardLayout;
---
+++ miro-board/app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+const DashboardRootPage = () => {
+  return (
+    <div>
+      <h1>Dashboard Root Page</h1>
+    </div>
+  );
+};
+
+export default DashboardRootPage;
