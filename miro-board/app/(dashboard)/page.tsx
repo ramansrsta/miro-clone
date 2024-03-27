979046ebf943a6d032a41bc4925763c93b60c72e
rmnsrsta <ramanshrestha@varicon.com.au>
2024-03-27 16:18:24 +0545

    Added Empty States

--- miro-board/app/(dashboard)/page.tsx
+++ miro-board/app/(dashboard)/page.tsx
@@ -1,9 +1,26 @@
-const DashboardRootPage = () => {
+"use client";
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+  const { organization } = useOrganization();
   return (
-    <div>
-      <h1>Dashboard Root Page</h1>
+    <div className=" flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </div>
   );
 };
 
-export default DashboardRootPage;
+export default DashboardPage;
