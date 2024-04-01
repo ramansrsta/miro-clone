439ae7a2884e18db5097305a0d403eb652729598
rmnsrsta <ramanshrestha@varicon.com.au>
2024-04-01 21:39:22 +0545

    Loading added in room

---
+++ miro-board/app/board/[boardId]/_components/info.tsx
@@ -0,0 +1,13 @@
+export const Info = () => {
+  return (
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
+      TODO: Information About the board
+    </div>
+  );
+};
+
+Info.Skeleton = function InfoSkeletion() {
+  return (
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]" />
+  );
+};
