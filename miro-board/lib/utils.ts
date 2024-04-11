89d0ba0f456b3589c160bc7f2b77703575ea243b
rmnsrsta <ramanshrestha@varicon.com.au>
2024-04-11 17:44:41 +0545

    Added Toolbars

--- miro-board/lib/utils.ts
+++ miro-board/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = Array.from(
+  { length: 10 },
+  () => "#" + Math.floor(Math.random() * 16777215).toString(16)
+);
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }
