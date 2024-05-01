e9904d61986dd87b8581f587c20a8a319a9b5830
rmnsrsta <ramanshrestha@varicon.com.au>
2024-05-01 19:23:06 +0545

    Added Active States

--- miro-board/app/board/[boardId]/_components/canvas.tsx
+++ miro-board/app/board/[boardId]/_components/canvas.tsx
@@ -1,21 +1,45 @@
 "use client";
 
+import { useState } from "react";
+
+import { CanvasMode, CanvasState } from "@/types/canvas";
+
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
 
-import { useSelf } from "@/liveblocks.config";
+import {
+  useCanRedo,
+  useCanUndo,
+  useHistory,
+  useSelf,
+} from "@/liveblocks.config";
 
 interface CanvasProps {
   boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
